a3088bb0839e6e73b1ecf309a4fa326e52ec8dde
Sam <a>
2020-10-29 21:08:24 +0000

    Initial Map Layout

---
+++ Map/Tileset.tsx
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<
+tileset
+version = "1.4"
+tiledversion = "1.4.3"
+name = "Tileset"
+tilewidth = "64"
+tileheight = "64"
+tilecount = "64"
+columns = "8" >
+    < image
+source = "Tiles.png"
+width = "512"
+height = "512" / >
+    < / tileset >
